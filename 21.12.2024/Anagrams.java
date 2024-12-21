8ba1092f8b6089e00483be42d94b24092bbcccf4
SAKTHIVEL_E <148361514+SAKTHIVEL280@users.noreply.github.com>
2024-12-21 18:53:39 +0530

    Update Anagrams.java

--- 21.12.2024/Anagrams.java
+++ 21.12.2024/Anagrams.java
@@ -5,20 +5,13 @@ public class Solution {
     static boolean isAnagram(String a, String b) {
         a = a.toLowerCase();
         b = b.toLowerCase();
-        
-        // If lengths are different, they cannot be anagrams
         if (a.length() != b.length()) {
             return false;
         }
-        
-        // Sort both strings
         char[] aArray = a.toCharArray();
         char[] bArray = b.toCharArray();
-        
         java.util.Arrays.sort(aArray);
         java.util.Arrays.sort(bArray);
-        
-        // Compare the sorted arrays
         return java.util.Arrays.equals(aArray, bArray);
     }
 
