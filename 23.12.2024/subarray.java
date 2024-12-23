3f8201f3ad03a30e0b32bfe2c9fb3237e9dd70c5
SAKTHIVEL_E <148361514+SAKTHIVEL280@users.noreply.github.com>
2024-12-23 19:43:44 +0530

    Create subarray.java

---
+++ 23.12.2024/subarray.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int negativeSubarrayCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum < 0) {
+                    negativeSubarrayCount++;
+                }
+            }
+        }
+
+        System.out.println(negativeSubarrayCount);
+        scanner.close();
+    }
+}
